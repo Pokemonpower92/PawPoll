88d2df0aee2c27973af55d1cc853db7e32ef8b06
jcarmac3 <pokemonpower92@gmail.com>
2021-08-03 00:52:19 -0400

    Preparing for deployment

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ const db = require('./db');
 
 const pollRouter = require('./routes/pollRouter');
 
+app.use(express.static(path.join(__dirname, "client", "build")))
 app.use(express.urlencoded( {extended: true }));
 app.use(cors());
 app.use(express.json());
@@ -26,6 +27,10 @@ app.get('/', (req, res) => {
 })
 
 app.use('/api', pollRouter);
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 app.listen(apiPort, () => {
     console.log(`PAWPOLL LISTENING ON PORT ${apiPort}`)

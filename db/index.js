513f845eebf7e5d51d64982c737805da51497e1d
jcarmac3 <pokemonpower92@gmail.com>
2021-08-03 01:33:43 -0400

    Production

--- db/index.js
+++ db/index.js
@@ -2,7 +2,7 @@ require("dotenv").config()
 const mongoose = require('mongoose');
 const { send } = require('process');
 
-mongoose.connect(process.env.MONGODB_CONNECTION_STRING, {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => {
         console.log("CONNECTION ESTABLISHED TO PAWPOLLDB");
     })
